4eb3c084c6e9edfe26a23bbf7f8ad4831bd339af
shivamsingh4120 <shivam.singh_cs24@gla.ac.in>
2025-04-29 18:00:35 +0530

    Update calc.h

--- calc.h
+++ calc.h
@@ -1,9 +1,14 @@
-int add(int x,int y){
-	return x+y;
+typedef int (*operation_func)(int, int);
+int add(int x, int y) {
+    return x + y;
 }
-int sub(int x, int y){
-	return x-y;
+int sub(int x, int y) {
+    return x - y;
 }
-int div(int x, int y){
-	return x/y;
+int divide(int x, int y) {
+    return x / y;
 }
+operation_func op_add = add;
+operation_func op_sub = sub;
+operation_func op_div = divide;
+
